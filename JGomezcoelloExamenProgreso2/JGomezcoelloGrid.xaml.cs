94715bad42c407991b0936478f1bf337b16c2ffe
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-12-03 07:34:47 -0500

    creacion de elementos para pagina de recarga y grid, al igual que en app shell creacion de tabs y flyout

---
+++ JGomezcoelloExamenProgreso2/JGomezcoelloGrid.xaml.cs
@@ -0,0 +1,9 @@
+namespace JGomezcoelloExamenProgreso2;
+
+public partial class JGomezcoelloGrid : ContentPage
+{
+	public JGomezcoelloGrid()
+	{
+		InitializeComponent();
+	}
+}
---
+++ JGomezcoelloExamenProgreso2/JGomezcoelloRecarga.xaml.cs
@@ -0,0 +1,9 @@
+namespace JGomezcoelloExamenProgreso2;
+
+public partial class JGomezcoelloRecarga : ContentPage
+{
+	public JGomezcoelloRecarga()
+	{
+		InitializeComponent();
+	}
+}
