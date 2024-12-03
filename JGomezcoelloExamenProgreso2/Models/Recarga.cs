21614da02469ad3fb8d4ed1c34b1afd944ac8c51
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-12-03 07:48:50 -0500

    Creacion de carpeta Models para agregar la clase de RecargaModel donde aqui se utilizara las variables de telefono, nombre y ultima recarga

---
+++ JGomezcoelloExamenProgreso2/Models/Recarga.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGomezcoelloExamenProgreso2.Models
+{
+    public class Recarga : INotifyPropertyChanged
+    {
+        private string _telefono;
+        private string _nombre;
+        private string _ultimaRecarga;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Telefono
+        {
+            get => _telefono;
+            set
+            {
+                _telefono = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string UltimaRecarga
+        {
+            get => _ultimaRecarga;
+            set
+            {
+                _ultimaRecarga = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
